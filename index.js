99a7d7850b7d61da46728d01b30369e817b09b22
AyushiBadika <badikaayushi@gmail.com>
2024-09-09 00:00:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import shortid from "shortid";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 mongoose.connect("mongodb+srv://badikaayushi:grfcAOQqlOyu0p8b@urlshortner.grrdf.mongodb.net/urlShortner");
 
@@ -17,30 +19,43 @@ const urlSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  visitHistory: [{ timestamp: { type: Number } }],
+  visitHistory: { type: Number },
 });
 
 const Url = mongoose.model("url", urlSchema);
 
 app.post("/shorten", async (req, res) => {
-  const { url } = req.body;
+  let { url } = req.body;
+
   if (!url) return res.status(400).json({ error: "url is required" });
 
-  const shortId = shortid.generate();
-  await Url.create({
-    shortId: shortId,
-    redirectUrl: url,
-    visitHistory: [],
-  });
-  res.status(200).json({ id: shortId });
+  if (!/^https?:\/\//i.test(url)) {
+    url = "http://" + url;
+  }
+
+  const findUrl = await Url.findOne({ redirectUrl: url });
+
+  if (findUrl) {
+    res.status(200).json({ url: `http://localhost:3000/${findUrl.shortId}` });
+  } else {
+    const shortId = shortid.generate();
+
+    await Url.create({
+      shortId: shortId,
+      redirectUrl: url,
+      visitHistory: 0,
+    });
+    res.status(200).json({ url: `http://localhost:3000/${shortId}` });
+  }
 });
 
 app.get("/:shortUrl", async (req, res) => {
-  console.log("sdfdasd", req.params);
   const url = await Url.findOne({ shortId: req.params.shortUrl });
-  console.log("tyui", url);
+
   if (url) {
-    res.redirect(url.redirectUrl);
+    url.visitHistory += 1;
+    await url.save();
+    res.redirect(301, url.redirectUrl);
   } else {
     res.status(404).send("URL not found");
   }
